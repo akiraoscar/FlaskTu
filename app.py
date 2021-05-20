b68e853fa858d402f96c35d4f135587ea11bc29c
akira <akira@alterra.id>
2021-05-20 10:10:01 +0700

    menambahkan jwt

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+from flask_mail import Mail
+
 from database.db import initialize_db
 from flask_restful import Api
-from resources.routes import initialize_routes
-
+from resources.errors import errors
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
+
+# imports requiring app and mail
+from resources.routes import initialize_routes
+
 api = Api(app)
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
@@ -13,5 +25,3 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 initialize_routes(api)
-
-app.run()
