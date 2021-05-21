a01024a3da048a22011d95c6da23d0e2c7e90d19
akira <akira@alterra.id>
2021-05-21 16:17:29 +0700

    mencoba menggunakan jwt

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
 from database.db import initialize_db
 from flask_restful import Api
 from resources.routes import initialize_routes
+from resources.errors import errors
 
 
 app = Flask(__name__)
-api = Api(app)
+app.debug = True
+app.config.from_envvar('ENV_FILE_LOCATION')
+api = Api(app, errors=errors)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
